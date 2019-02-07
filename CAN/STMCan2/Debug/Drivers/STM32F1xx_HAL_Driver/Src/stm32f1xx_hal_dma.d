6b103bb8f0955d5d0eda3ec8d03fe3524012b955
virginiasatyro <vivi_satyro@hotmail.com>
2019-02-07 14:33:36 -0200

    atualização dos códigos de envio via CAN

---
+++ CAN/STMCan2/Debug/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.d
@@ -0,0 +1,71 @@
+Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.o: \
+ ../Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c \
+ C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
+ C:/Users/vivi_/Desktop/STMCan2/STMCan2/Inc/stm32f1xx_hal_conf.h \
+ C:/Users/vivi_/Desktop/STMCan2/STMCan2/Inc/main.h \
+ C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
+ C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h \
+ C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/CMSIS/Include/core_cm3.h \
+ C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h \
+ C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h \
+ C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h \
+ C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h \
+ C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h \
+ C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h \
+ C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_can.h \
+ C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_can_ex.h \
+ C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h \
+ C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
+ C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
+ C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h
+
+C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
+
+C:/Users/vivi_/Desktop/STMCan2/STMCan2/Inc/stm32f1xx_hal_conf.h:
+
+C:/Users/vivi_/Desktop/STMCan2/STMCan2/Inc/main.h:
+
+C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h:
+
+C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h:
+
+C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
+
+C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+
+C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/CMSIS/Include/core_cm3.h:
+
+C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/CMSIS/Include/core_cmInstr.h:
+
+C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
+
+C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h:
+
+C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h:
+
+C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h:
+
+C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h:
+
+C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h:
+
+C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_can.h:
+
+C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_can_ex.h:
+
+C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h:
+
+C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h:
+
+C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
+
+C:/Users/vivi_/Desktop/STMCan2/STMCan2/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
